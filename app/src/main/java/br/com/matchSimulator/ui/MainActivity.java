49fc5938592791dfad3d4b84f56c508386406f28
Danilo Gomes <114317159+dgomesdev@users.noreply.github.com>
2023-01-03 20:22:46 -0400

    Update MainActivity.java

--- app/src/main/java/br/com/matchSimulator/ui/MainActivity.java
+++ app/src/main/java/br/com/matchSimulator/ui/MainActivity.java
@@ -30,11 +30,11 @@ public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
     private MatchesApi matchesAPI;
-    private MatchesAdapter matchesAdapter = new MatchesAdapter(Collections.emptyList());
+    private MatchesAdapter matchesAdapter;
 
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
@@ -59,7 +59,6 @@ public class MainActivity extends AppCompatActivity {
     private void setupMatchesList() {
         binding.rvMatches.setHasFixedSize(true);
         binding.rvMatches.setLayoutManager(new LinearLayoutManager(this));
-        binding.rvMatches.setAdapter(matchesAdapter);
         findMatchesFromAPI();
     }
 
@@ -68,18 +67,20 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void setupFloatingActionButton() {
-        binding.fabSimulate.setOnClickListener(view -> view.animate().rotation(360).setDuration(500).setListener(new AnimatorListenerAdapter() {
+        binding.fabSimulate.setOnClickListener(view -> {
+            view.animate().rotation(360).setDuration(500).setListener(new AnimatorListenerAdapter() {
             @Override
             public void onAnimationEnd(Animator animation) {
-                Random random = new Random();
                 for (int i = 0; i < matchesAdapter.getItemCount(); i++){
                     Match match = matchesAdapter.getMatches().get(i);
+                    Random random = new Random();
                     match.getHomeTeam().setScore(random.nextInt(match.getHomeTeam().getStars() + 1));
                     match.getAwayTeam().setScore(random.nextInt(match.getAwayTeam().getStars() + 1));
                     matchesAdapter.notifyItemChanged(i);
                 }
             }
-        }));
+            }).start();
+        });
     }
 
     private void showErrorMessage() {
@@ -92,8 +93,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onResponse(@NonNull Call<List<Match>> call, @NonNull Response<List<Match>> response) {
                 if (response.isSuccessful()) {
-                    List<Match> matches = response.body();
-                    matchesAdapter = new MatchesAdapter(matches);
+                    matchesAdapter = new MatchesAdapter(response.body());
                     binding.rvMatches.setAdapter(matchesAdapter);
                 }
                 else {showErrorMessage();}
